cf12a98ff7de27d801187fc27de5463407ba2fe9
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2023-08-15 16:49:02 -0700

    References

--- main.c
+++ main.c
@@ -1,22 +1,14 @@
 #include <stdio.h>
-#include <string.h>
-
-// print Fahrenheit-Celsius table
-// for fahr = 0, 20, ..., 300
 
 int main()
 {
-    // Length of array is 6, length of string is 5
-    char dest[6] = "Hello";
-    // Length of string to append to first string is also 5
-    char src[20] = "World";
-    printf("LEN: %lu\tSIZE: %lu\n", strlen(dest), sizeof(dest));
+    int num = 5;
+    int *nref = &num;
+    *nref = 6;
 
-    // Behavior is undefined when appending a string to another string that is not large enough
-    strncat(dest, src, 5);
-    printf("LEN: %lu\tSIZE: %lu\n", strlen(dest), sizeof(dest));
-
-    // Getting char at index 9 still works but is undefined behavior, due to original array not being this big
-    printf("TEST: %c\n", dest[9]);
+    printf("%d\n", num);
+    printf("%d\n", *nref);
+    printf("%x\n", &num);
+    printf("%x\n", nref);
 }
 
