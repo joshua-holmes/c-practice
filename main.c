fee2d78823f9766e8d7bfb2b0ec3f7754cc01927
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2023-08-16 19:04:44 -0700

    Passing a value to a func copies the value, unless it's a ref

--- main.c
+++ main.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 
+void change_val_w_ref(int *num) {
+    *num = 6;
+    printf("val changed to 6: %d\n", *num);
+}
+
+void change_val_no_ref(int num) {
+    num = 6;
+    printf("val changed to 6: %d\n", num);
+}
+
 int main()
 {
     int num = 5;
-    int *nref = &num;
-    *nref = 6;
-
-    printf("%d\n", num);
-    printf("%d\n", *nref);
-    printf("%x\n", &num);
-    printf("%x\n", nref);
+    printf("val is 5: %d\n", num);
+    change_val_no_ref(num);
+    printf("val after change: %d\n", num);
+    change_val_w_ref(&num);
+    printf("val after change: %d\n", num);
 }
 
